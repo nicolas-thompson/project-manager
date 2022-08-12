619fece10cd47fce7a6d1402530afec8df150ed4
Nicolas Thompson <mail@nicolasthompson.com>
2022-08-12 09:53:27 +0100

    Switches to use ES modules

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,17 +1,15 @@
-namespace App {
-  // Project Status
-  export enum ProjectStatus {
-    Active,
-    Finished,
-  }
+// Project Status
+export enum ProjectStatus {
+  Active,
+  Finished,
+}
 
-  export class Project {
-    constructor(
-      public id: string,
-      public title: string,
-      public description: string,
-      public people: number,
-      public status: ProjectStatus
-    ) {}
-  }
+export class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
 }
